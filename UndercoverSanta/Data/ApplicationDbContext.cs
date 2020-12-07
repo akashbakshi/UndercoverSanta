75bc0db812814a3eaf22cb8a27163b7796fb74b6
AkashBakshi-SurfacePro <akash1996@hotmail.com>
2020-12-07 18:46:36 -0500

    Add project files.

---
+++ UndercoverSanta/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UndercoverSanta.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+            
+        }
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var endpoint = "https://172.28.79.206:8902/";
+            var key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+            optionsBuilder.UseCosmos(endpoint, key, "UndercoverSantaDb").UseLoggerFactory(GenerateLoggerFactory()).EnableSensitiveDataLogging(true);
+        }
+
+        private ILoggerFactory GenerateLoggerFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder.AddConsole().AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Trace));
+
+            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
+        */
+    }
+}
